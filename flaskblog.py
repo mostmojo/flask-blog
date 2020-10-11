8891df4b2a7ca2804cdcb3f5db145c82fe404457
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-10-12 00:01:50 +0300

    Add styling

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Mojo Dojo',
+        'title': 'The Art of Code',
+        'content': 'Coding is an artform as you put together different elements to build a website.',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Meila Feila',
+        'title': 'The Sus Chronicles',
+        'content': 'Did you vent? I saw you vent and lurk around medbay.',
+        'date_posted': 'April 25, 2020'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':

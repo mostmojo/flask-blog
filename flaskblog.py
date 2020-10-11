9e672108b1eaa8529a49f72a6a06f5795f4d967b
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-10-11 21:41:16 +0300

    Add new items

--- flaskblog.py
+++ flaskblog.py
@@ -3,5 +3,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    return '<h1>Home page</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

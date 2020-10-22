c7010a195cdce968c354a67d6f5f271081e8339c
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-10-22 22:56:53 +0300

    Add new files

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '93634a0c5bba68ecd50c3bcb2ef47bf9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
